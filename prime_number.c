9461383e0b1cfb9b0584316a05e9138a7d68bc77
Marshey <platecdust@gmail.com>
2020-07-02 16:06:49 +0900

    ごみの排除

--- prime_number.c
+++ prime_number.c
@@ -3,19 +3,8 @@
 
 void main() {
 	int a, i, d;
+	
 	scanf("%d", &a);
-<<<<<<< HEAD
-	if(a%2 == 0) {
-		printf("%d は偶数\n", a);
-	} else {
-		d = 0;
-		for(i = 3; i <= sqrt(2); i++) {
-			if(a % i == 0) {
-				d = i;
-			}
-		}
-		if(d == 0) {
-=======
 	if (a % 2 == 0) {
 		printf("%d は偶数\n", a);
 	} else {
@@ -26,14 +15,9 @@ void main() {
 			}
 		}
 		if (d == 0) {
->>>>>>> 2018.11.22
 			printf("%d は素数である\n", a);
 		} else {
 			printf("%d は %d で割り切れる\n", a, d);
 		}
-<<<<<<< HEAD
-	} 
-=======
 	}
->>>>>>> 2018.11.22
 }
