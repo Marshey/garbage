3456e383f2f9a9074925ddcdb2ed9d5d6149de61
Marshey <platecdust@gmail.com>
2020-07-09 11:57:42 +0900

    ごみの排除

--- 3sort.c
+++ 3sort.c
@@ -1,38 +1,26 @@
+//繰り返し処理を使わずに3つの整数を昇順に並び替えようとした糞プログラム
+
 #include <stdio.h>
 
 void main() {
 	int a, b, c;
+
 	scanf("%d %d %d", &a, &b, &c);
-<<<<<<< HEAD
-	if(b < c) {
-		if(a < b) {
-			printf("%d %d %d", a, b, c);
-		} else {
-			if(a < c) {
-=======
 	if (b < c) {
 		if (a < b) {
 			printf("%d %d %d", a, b, c);
 		} else {
 			if (a < c) {
->>>>>>> 2018.11.22
 				printf("%d %d %d", b, a, c);
 			} else {
 				printf("%d %d %d", b, c, a);
 			}
 		}
 	} else {
-<<<<<<< HEAD
-		if(a < c) {
-			printf("%d %d %d", a, c, b);
-		} else {
-			if(a < b) {
-=======
 		if (a < c) {
 			printf("%d %d %d", a, c, b);
 		} else {
 			if (a < b) {
->>>>>>> 2018.11.22
 				printf("%d %d %d", c, a, b);
 			} else {
 				printf("%d %d %d", c, b, a);
